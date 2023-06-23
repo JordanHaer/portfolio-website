1bea3458336b8f91c1da3bd1485c84d69909e4f4
JordanHaer <jordanhaer@gmail.com>
2023-06-23 10:13:53 +0100

    Bio changes

--- src/app/home/LeftSide/Bio.tsx
+++ src/app/home/LeftSide/Bio.tsx
@@ -2,9 +2,9 @@ export default function Bio() {
   return (
     <>
       <h1 className="text-5xl font-bold leading-tight tracking-tight text-slate-200 sm:text-6xl">Jordan Haer</h1>
-      <h2 className="mt-4 text-xl font-medium tracking-tight text-slate-200 sm:text-2xl">Web Developer</h2>
+      <h2 className="mt-4 text-xl font-medium tracking-tight text-slate-200 sm:text-2xl">Software Developer</h2>
       <p className="mt-4 max-w-xs leading-normal">
-        Passionate about building full stack web applications that combine intuitive interfaces with powerful
+        Passionate about building full stack applications that combine intuitive interfaces with powerful
         functionality.
       </p>
     </>
