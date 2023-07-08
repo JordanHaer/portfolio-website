acfa7a338de34f6a48e0aafea46d89731418b086
JordanHaer <jordanhaer@gmail.com>
2023-07-08 13:10:31 +0100

    About section content changes

--- src/app/home/RightSide/About/AboutParagraph.tsx
+++ src/app/home/RightSide/About/AboutParagraph.tsx
@@ -1,31 +1,18 @@
 export default function AboutParagraph() {
+  const aboutContent: string[] = [
+    "When I began my coding journey, I enrolled in Harvard's CS50 Python course to establish a solid foundation in computer science. This course proved invaluable as it covered the fundamentals of programming and explored topics such as object-oriented-programming and data structures and algorithms.",
+    "To further advance my skills, I joined a full-stack developer bootcamp, diving into web fundamentals and and gained hands-on experience in building applications using the MERN stack (MongoDB, Express.js, React, Node.js).",
+    "Applying my newfound knowledge, I developed MockStocks, a simulated trading platform. Through this project, I honed my skills in system design, backend development, user authentication, and frontend design.",
+    "Continuously learning and staying updated with new tools, technologies, and concepts is an integral part of my journey as a developer, most recentcly learning Next.js as well as TypeScript. I am committed to expanding my knowledge base and further progressing my skills.",
+  ];
+
   return (
-    <div className="text-slate-300 font-medium">
-      <p className="mb-4">
-        When learning to code I was quickly overwhelmed by the amount of information available online. To get
-        started, I followed the recommendations of various sources and enrolled in Harvard&apos;s online CS50
-        course, specifically their Python program. This experience allowed me to grasp the fundamentals of
-        programming such as data structures and object-oriented programming.
-      </p>
-      <p className="mb-4">
-        Wanting to build upon the solid foundation I gained from completing the CS50 Python course and create more
-        advanced applications I enrolled in a full-stack developer bootcamp. Once learning the Web fundementals
-        HTML, CSS and JavaScript, I delved into the MERN stack.
-      </p>
-      <p className="mb-4">
-        Applying my newfound knowledge, I built a practice stock trading platform called MockStocks. This project
-        allowed me to enhance my skills in system design, develop robust back-end systems, implement secure user
-        authentication, and design intuitive front-end interfaces.
-      </p>
-      <p className="pb-4">
-        Continuously learning and staying updated with new tools, technologies, and concepts is an integral part of
-        my journey as a developer, I have recently learnt Next.js as well as TypeScript. I am committed to
-        expanding my knowledge base and embracing the ever-evolving landscape of technology.
-      </p>
+    <div className="font-medium text-slate-300">
+      {aboutContent.map((paragraph, index) => (
+        <p className="pb-4" key={index}>
+          {paragraph}
+        </p>
+      ))}
     </div>
   );
-}
-
-{
-  /* <span className="font-medium text-slate-200">Korok seeds</span> */
 }
